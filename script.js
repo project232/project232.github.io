c9c6dc4648d2a820b945a5b697f437b092db09e4
project232 <147293332+project232@users.noreply.github.com>
2023-10-30 18:30:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,20 @@ const heightInput = document.getElementById('height');
 const generateImageButton = document.getElementById('generateimage');
 const randomImageElement = document.getElementById('random-image');
 const customCategoryInput = document.getElementById('custom-category');
+document.getElementById("generateimage").addEventListener("click", function() {
+    const element = document.documentElement; // Get the root element of the document
 
+    // Check if the current document supports the Fullscreen API
+    if (element.requestFullscreen) {
+        element.requestFullscreen(); // Request full-screen mode
+    } else if (element.mozRequestFullScreen) { // Firefox
+        element.mozRequestFullScreen();
+    } else if (element.webkitRequestFullscreen) { // Chrome, Safari and Opera
+        element.webkitRequestFullscreen();
+    } else if (element.msRequestFullscreen) { // IE/Edge
+        element.msRequestFullscreen();
+    }
+});
 generateImageButton.addEventListener('click', () => {
     const customCategory = customCategoryInput.value.trim();
     const customWidth = widthInput.value;
